f4ebb04dd5fd02ffa4a53c696b7eb8d3c8711604
Erik Heinzle <eh.gamedev@outlook.com>
2024-10-20 14:23:50 +0200

    CameraPawn and selecting + moving the WorkerCharacter works together

--- Source/BlueCollarTown/Public/MainCamera.h
+++ Source/BlueCollarTown/Public/MainCamera.h
@@ -3,8 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "MainPlayerController.h"
 #include "GameFramework/Pawn.h"
 #include "MainCamera.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
 
 UCLASS()
 class BLUECOLLARTOWN_API AMainCamera : public APawn
@@ -12,18 +17,113 @@ class BLUECOLLARTOWN_API AMainCamera : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	AMainCamera();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
+	
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void GetTerrainPosition(FVector& TerrainPosition) const;
+	
+	UFUNCTION()
+	void Forward(float AxisValue);
+
+	UFUNCTION()
+	void Right(float AxisValue);
+	
+	UFUNCTION()
+	void Zoom(float AxisValue);
+	
+	UFUNCTION()
+	void RotateRight();
+	
+	UFUNCTION()
+	void RotateLeft();
+	
+	UFUNCTION()
+	void EnableRotate();
+
+	UFUNCTION()
+	void DisableRotate();
+
+	UFUNCTION()
+	void RotateHorizontal(float AxisValue);
+
+	UFUNCTION()
+	void RotateVertical(float AxisValue);
+
+	UFUNCTION()
+	void EdgeScroll();
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Camera")
+	float MoveSpeed = 15.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Camera")
+	float RotateSpeed = 2.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Camera")
+	float RotatePitchMin = 10.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Camera")
+	float RotatePitchMax = 80.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Camera")
+	float ZoomSpeed = 2.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Camera")
+	float MinZoom = 500.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Camera")
+	float MaxZoom = 4000.0f;
+
+	// Mouse Input
+
+	UFUNCTION()
+	AActor* GetSelectedObject();
+	
+	UFUNCTION()
+	void MouseLeftPressed();
+	
+	UFUNCTION()
+	void MouseLeftReleased();
+	
+	UFUNCTION()
+	void MouseRightPressed();
+	
+	UFUNCTION()
+	void MouseRightReleased();
+	
+	UPROPERTY()
+	AMainPlayerController* MainPlayerController;
+
+private:
+
+	UFUNCTION()
+	void CameraBounds();
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	USceneComponent* SceneComponent;
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY()
+	FVector TargetLocation;
+
+	UPROPERTY()
+	FRotator TargetRotation;
+	
+	UPROPERTY()
+	float TargetZoom;
+	
+	UPROPERTY()
+	bool CanRotate;
+	
 };
